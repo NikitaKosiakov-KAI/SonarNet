eca159383fa0142eae98dba7c48e99da7af46a73
NK-KAI <8666975@stud.kai.edu.ua>
2026-05-11 16:27:13 +0300

    Додано нову фічу: NewFeature

---
+++ SonarNet/NewFeature.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SonarNet
+{
+    public class NewFeature
+    {
+        // Простий метод для демонстрації нової фічі
+        public string GetFeatureDescription()
+        {
+            return "Це нова фіча, додана для перевірки конвеєру GitHub Actions.";
+        }
+    }
+}
